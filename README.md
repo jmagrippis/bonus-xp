622169ac622ed3373de923d372703539dced3521
Johnny Magrippis <j@magrippis.com>
2023-11-29 14:12:27 +0100

    ✅  Setup Playwright for browser tests

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Bonus XP
 
+![E2E](https://github.com/jmagrippis/bonus-xp/actions/workflows/playwright.yaml/badge.svg)
+
 Learning to code... By exploring 👩‍🚀
---
+++ tests/navigation.spec.ts
@@ -0,0 +1,33 @@
+import {test, expect} from '@playwright/test'
+
+test('navigation smoke test', async ({page}) => {
+	// start at the home page
+	await page.goto('/')
+
+	await expect(page).toHaveTitle(/Bonus XP/)
+
+	// navigate to Courses
+	await page.getByRole('heading', {name: 'courses', level: 2}).click()
+	// should get you straight to the only course, SvelteKit/Learn
+	await expect(page).toHaveTitle(/SvelteKit\/Learn/)
+	await expect(
+		page.getByRole('heading', {
+			name: 'Start building with SvelteKit',
+			level: 1,
+		}),
+	).toBeVisible()
+
+	// navigate back home
+	await page.getByRole('link', {name: 'Bonus XP'}).click()
+	await expect(page).toHaveTitle(/Bonus XP/)
+
+	// navigate to Play
+	await page.getByRole('heading', {name: 'play', level: 2}).click()
+	await expect(page).toHaveTitle(/Play/)
+	await expect(
+		page.getByRole('heading', {
+			name: 'Play',
+			level: 1,
+		}),
+	).toBeVisible()
+})
